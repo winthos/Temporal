c51cdd9796861b277b27599000123b9fea240702
LIGHTS\k.harris <k.harris@digipen.edu>
2016-10-31 09:51:50 -0700

    planets colored at random.
    color switching not quite in yet.

---
+++ Space-Time/Assets/Scripts/ColorLogic.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Written by Kaila Harris
+ * Goes on every visible object in the game
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ColorLogic : MonoBehaviour
+{
+    [SerializeField]
+    bool RandomizedColors = false;
+
+    Renderer rend; //Renderer of our Cube
+    float transitionTime = 5f;
+
+    GameObject ColorManager;
+    ColorPalette pinkTriad;
+
+    void Start()
+    {
+        ColorManager = GameObject.Find("LevelGlobals");
+        pinkTriad = ColorManager.GetComponent<ColorManager>().palPinkTriad;
+
+        rend = GetComponent<Renderer>();
+
+        if (gameObject.tag == "Planet")
+            SetPlanetColors(pinkTriad.GetPlanetColors());
+
+    }
+
+    void Update()
+    {
+    }
+
+    void SetPlanetColors(List<Color> _list)
+    {
+        int planCol = Random.Range(0, (_list.Count));
+        SetObjectColor(_list[planCol]);
+        _list.RemoveAt(planCol);
+
+        foreach (Transform child in transform)
+        {
+            int num = Random.Range(0, (_list.Count));
+            child.gameObject.GetComponent<ColorLogic>().SetObjectColor(_list[num]);
+        }
+    }
+
+    void SetObjectColor(Color _color)
+    {
+        rend.material.SetColor("_Color", _color);
+    }
+}
---
+++ Space-Time/Assets/Scripts/ColorManager.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Written by Kaila Harris
+ * Goes on the Level Globals or Game Manager object
+ */
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ColorManager : MonoBehaviour
+{
+    public ColorPalette palPinkTriad = new ColorPalette(); 
+
+    // Use this for initialization
+    void Awake ()
+    {
+        SetPinkTriad();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void SetPinkTriad()
+    {
+        palPinkTriad.SetPlanetsAndSatellites(
+            new Color(0.5882352941176471f, 0.07058823529411765f, 0.6980392156862745f),
+            new Color(1.0f, 0.8901960784313725f, 0.6078431372549019f),
+            new Color(0.9137254901960784f, 0.5058823529411764f, 1.0f),
+            new Color(0.3254901960784314f, 0.8f, 0.6431372549019608f),
+            new Color(0.3176470588235294f, 0.6980392156862745f, 0.5725490196078431f));
+    }
+}
+
+// gray skybox  A1A7A705
+//skybox  	#c2ab4a; 76.1, 67.1, 29
