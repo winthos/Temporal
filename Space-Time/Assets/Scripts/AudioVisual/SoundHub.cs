b21474c84174c712b327f304e3acf67d6136868a
LightsAct2/k-harris <kharris987@gmail.com>
2017-01-13 10:36:54 -0800

    sound still not working but this is backup
    getting there

--- Space-Time/Assets/Scripts/AudioVisual/SoundHub.cs
+++ Space-Time/Assets/Scripts/AudioVisual/SoundHub.cs
@@ -7,8 +7,51 @@ using UnityEngine;
 using AudioVisualization;
 using System.Collections;
 
-public class SoundController : MonoBehaviour
+
+
+public class SoundHub : MonoBehaviour
 {
+    // Static instance
+    static SoundHub _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            _instance.Initialize();
+        }
+
+    }
+
+    void Initialize()
+    {
+        // programatically load audio clips
+    }
+
+    public static SoundHub GetInstance()
+    {
+        return _instance;
+    }
+
+
+    [SerializeField]
+    AudioClip bgm;
+    [SerializeField]
+    AudioClip gunshot;
+
+    // Use this for initialization
+    void Start()
+    {
+        AudioVisualManager.PlayBGM(bgm, false, 0);
+    }
+
+    public static void FireWeapon(GameObject _obj)
+    {
+        AudioVisualManager.PlaySFX(GetInstance().gunshot, _obj, 1);
+    }
+
+
     //load audio clips into AVM
 
     //player damage
