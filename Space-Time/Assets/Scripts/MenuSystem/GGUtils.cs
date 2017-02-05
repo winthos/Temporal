fd993d58c7181e1fea784ec3cb2a004babee1f9e
LightsAct2/k-harris <kharris987@gmail.com>
2017-02-04 19:05:01 -0800

    Nolan working on the menu

---
+++ Space-Time/Assets/Scripts/MenuSystem/GGUtils.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Object = UnityEngine.Object;
+
+public enum Side
+{
+    Up,
+    Left,
+    Right,
+    Down
+}
+
+public enum Quadrant
+{
+    UpperLeft,
+    UpperRight,
+    LowerLeft,
+    LowerRight,
+}
+
+public static class GGUtils
+{
+  // Fill an array with a value
+  public static void Populate<T>(this T[] arr, T val)
+  {
+    for (int i = 0; i < arr.Length; i++)
+    {
+      arr[i] = val;
+    }
+  }
+
+  // Turn a 1D array into a 2D array
+  public static T[,] Make2D<T>(T[] input, int height, int width)
+  {
+    T[,] output = new T[height, width];
+    for (int i = 0; i < height; i++)
+    {
+      for (int j = 0; j < width; j++)
+      {
+        output[i, j] = input[i*width + j];
+      }
+    }
+    return output;
+  }
+
+  public static bool SetInstance<T>(ref T target, T instance) where T : MonoBehaviour
+  {
+    if (Object.ReferenceEquals(target, null))
+    {
+      target = instance;
+      return true;
+    }
+    else if (!Object.ReferenceEquals(target, instance))
+    {
+      Object.Destroy(instance.gameObject);
+      return false;
+    }
+    return false;
+  }
+}
+
+public class SingletonBehavior<T> : MonoBehaviour where T : MonoBehaviour
+{
+  public static T Instance;
+
+  protected virtual void Awake()
+  {
+    GGUtils.SetInstance(ref Instance, this as T);
+  }
+}
+
+public class Pair<X, Y>
+{
+  public Pair()
+  {
+  }
+
+  public Pair(X first, Y second)
+  {
+    this.First = first;
+    this.Second = second;
+  }
+
+  public X First { get; set; }
+  public Y Second { get; set; }
+};
+
+public class Ref<T>
+{
+  private readonly Func<T> getter;
+  private readonly Action<T> setter;
+
+  public Ref(Func<T> getter, Action<T> setter)
+  {
+    this.getter = getter;
+    this.setter = setter;
+  }
+
+  public T Value
+  {
+    get { return getter(); }
+    set { setter(value); }
+  }
+}
---
+++ Space-Time/Assets/Scripts/MenuSystem/PausableState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PausableState : MonoBehaviour
+{
+  public bool CanPause = false;
+  // Use this for initialization
+  void Start()
+  {
+    MenuHelper.Instance.CanPause = this.CanPause;
+  }
+
+  void Awake()
+  {
+    MenuHelper.Instance.CanPause = this.CanPause;
+  }
+
+    void OnApplicationFocus(bool hasFocus)
+    {
+        if(!MenuHelper.Instance.IsPaused())
+        {
+            MenuHelper.Instance.Pause();
+        }
+    }
+}
