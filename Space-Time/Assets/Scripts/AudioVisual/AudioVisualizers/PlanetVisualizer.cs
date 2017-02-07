b7bff79ae98287e6688c17819f243cfa13e70485
LightsAct2/k-harris <kharris987@gmail.com>
2017-02-07 00:51:19 -0800

    working on visualizer for planets

---
+++ Space-Time/Assets/Scripts/AudioVisual/AudioVisualizers/PlanetVisualizer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlanetVisualizer : MonoBehaviour
+{
+    float scaleMultiplier = 10;
+    //float sampleMultiplier = 40;
+    float timeMultiplier = 30;
+    Vector3 scaleMinimum;
+
+    private void Awake()
+    {
+        scaleMinimum = gameObject.transform.localScale;
+    }
+
+    void Start()
+    {        
+    }
+
+    void Update()
+    {
+        VisualizeScale();
+    }
+
+    void VisualizeScale()
+    {
+        float size = 0;
+        if (VisualizerHub.GetSamples() != null)
+            size = VisualizerHub.GetSamples()[4];
+
+        Vector3 previousScale = gameObject.transform.localScale;
+        Vector3 newScale = scaleMinimum + new Vector3(size, size, size) * 10;
+        gameObject.transform.localScale = Vector3.Lerp(previousScale, newScale, Time.deltaTime * 30);
+        gameObject.transform.localScale = newScale;
+
+        /*
+        Vector3 previousScale = gameObject.transform.localScale;
+        Vector3 newScale = scaleMinimum + new Vector3(size, size, size) * scaleMultiplier;
+        gameObject.transform.localScale = Vector3.Lerp(previousScale, newScale, Time.deltaTime * timeMultiplier);
+        gameObject.transform.localScale = newScale;
+        */
+    }
+}
---
+++ Space-Time/Assets/Scripts/AudioVisual/VisualizerHub.cs
@@ -0,0 +1,62 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//	Authors: Kaila Harris
+//	Copyright © 2017 DigiPen (USA) Corp. and its owners. All Rights Reserved.
+////////////////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
+using AudioVisualization;
+using System.Collections.Generic;
+
+
+public class VisualizerHub : MonoBehaviour
+{
+    // Static instance
+    static VisualizerHub _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            _instance.Initialize();
+        }
+    }
+    
+    public static VisualizerHub GetInstance()
+    {
+        return _instance;
+    }
+
+    //public List<Vector3> topSample;
+    //public Vector3[] trebleSamples;
+    //public Vector3[] mediumSamples;
+    //public  baseSamples;
+
+    int numberOfSamples;
+    float[] allSamples;
+
+    void Initialize()
+    {
+        numberOfSamples = 512;
+        allSamples = new float[numberOfSamples];
+    }
+    
+    void Update()
+    {
+        //Background Music
+        Visualization(SoundHub.source_bgm, numberOfSamples, 40, 30);
+    }
+
+    void Visualization(AudioSource _source, int _sampleNumber, float _sampleMultiplier, float _timeMultiplier)
+    {
+        float[] samples = new float[_sampleNumber];
+
+        _source.GetSpectrumData(samples, 0, FFTWindow.Hamming);
+        allSamples = samples;
+    }
+
+    public static float[] GetSamples()
+    {
+        return GetInstance().allSamples;
+    }
+}
