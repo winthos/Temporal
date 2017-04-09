26312889171504c7395e730cb2d331e6fab6d214
winthos <winson.whan@gmail.com>
2017-04-08 19:23:53 -0700

    cUT OUT EVERthing, this is the version that chagned ab unch

---
+++ Space-Time/Assets/BakudanDonePlaying.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BakudanDonePlaying : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(gameObject.GetComponent<ParticleSystem>().isPlaying == false)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
