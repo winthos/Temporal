df98fd5a2d03e314485d09f199f40496de14258a
LightsAct2/k-harris <kharris987@gmail.com>
2018-04-11 13:11:19 -0700

    changing out main menu

---
+++ Space-Time/Assets/Scripts/Menus/MenuButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public Sprite defaultSprite;
+    public Sprite selectedSprite;
+    public Color defaultColor;
+    public Color selectedColor;
+
+    private Button btn;
+    private Image img;
+
+    void Awake()
+    {
+        btn = GetComponent<Button>();
+        img = GetComponent<Image>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        btn.OnPointerEnter(MouseOver());
+
+        img.sprite = defaultSprite;
+        img.color = defaultColor;
+    }
+
+    private void MouseOver()
+    {
+        img.sprite = selectedSprite;
+        img.color = selectedColor;
+        Debug.Log("hover");
+    }
+
+    private void OnMouseExit()
+    {
+        img.sprite = defaultSprite;
+        img.color = defaultColor;
+    }
+}
